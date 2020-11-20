09cb273f7f42dbe802eecc0993ae7a11ea76c164
shinichi <shinichi@hotmail.nl>
2020-11-20 17:23:27 +0100

    [EDIT] Added lightbulb attributes

--- Project/app/src/main/java/com/csd/huelight/data/LightBulb.java
+++ Project/app/src/main/java/com/csd/huelight/data/LightBulb.java
@@ -1,7 +1,12 @@
 package com.csd.huelight.data;
 
 public class LightBulb {
-
-
+    private String UID;
+    private String name;
+    private boolean on;
+    private short hue;
+    private byte saturation;
+    private byte brightness;
+    private boolean colorLoop;
 
 }
