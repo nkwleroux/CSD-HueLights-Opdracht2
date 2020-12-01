72c56c5903351d7fff45d3aa3abb860d8828e7a7
Nicholas <nkw.leroux@student.avans.nl>
2020-12-02 00:54:08 +0100

    [ADDED] NavGraph and fixed few navigationView bugs

--- Project/app/src/main/java/com/csd/huelight/ui/mainactivity/MainActivity.java
+++ Project/app/src/main/java/com/csd/huelight/ui/mainactivity/MainActivity.java
@@ -1,17 +1,23 @@
 package com.csd.huelight.ui.mainactivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
@@ -24,31 +30,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //navigation drawer
-        navigationView = findViewById(R.id.nav_view);
+        this.navigationView = findViewById(R.id.nav_view);
+        this.navigationView.setNavigationItemSelectedListener(this);
         this.drawerLayout = findViewById(R.id.drawer_layout);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-        init();
-
-//        NavigationView navigationView = findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
-
-/*        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, this.drawer, toolbar,
+        toggle = new ActionBarDrawerToggle(this, this.drawerLayout, toolbar,
                 R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
-        toggle.syncState();*/
-
-//        navigationView.setCheckedItem(R.id.nav_list);
+        toggle.syncState();
 
         //Viewmodel
         LightBulbViewModel lightBulbViewModel = ViewModelProviders.of(this).get(LightBulbViewModel.class);
@@ -61,14 +60,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+    }
 
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Can be used to make a refresh button or something. Need to replace the inflater.
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.drawer_menu,menu);
+        return true;
+    }*/
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        init();
     }
 
     private void init(){
         NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment_container);
         NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout);
         NavigationUI.setupWithNavController(navigationView, navController);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -76,12 +105,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Log.d(LOGTAG, "onNavigationItemSelected called " + item.getItemId());
         switch (item.getItemId()){
             case R.id.nav_list:
-                Navigation.findNavController(this,R.id.nav_host_fragment_container).navigate(R.id.lightBulbListFragment);
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_container, new LightBulbListFragment()).commit();
+
+                // nav options to clear backstack
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.navigation_graph, true)
+                        .build();
+
+                Navigation.findNavController(this,R.id.nav_host_fragment_container).navigate(R.id.lightBulbListFragment,null,navOptions);
                 break;
             case R.id.nav_settings:
-                Navigation.findNavController(this,R.id.nav_host_fragment_container).navigate(R.id.settingsFragment);
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_container, new SettingsFragment()).commit();
+                if(isValidDestination(R.id.settingsFragment)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment_container).navigate(R.id.settingsFragment);
+                }
                 break;
         }
 
@@ -91,14 +126,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-/*    @Override
+    public boolean isValidDestination(int destination){
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment_container).getCurrentDestination().getId();
+    }
+
+    //TODO Clear backstack from. When in huelight then click on settings. Screen has to change to list not huelight.
+    @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START);
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
         }else {
+            if(Navigation.findNavController(this, R.id.nav_host_fragment_container).getCurrentDestination().getId() == R.id.settingsFragment){
+                if(navigationView.getCheckedItem() != null) {
+                    navigationView.getCheckedItem().setChecked(false);
+                }
+            }
             super.onBackPressed();
         }
-    }*/
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
